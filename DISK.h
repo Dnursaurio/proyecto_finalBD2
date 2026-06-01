75369b09f3e48983420542e9a0bc078048ba464f
andyvillanueva24 <143142019+andyvillanueva24@users.noreply.github.com>
2026-06-01 11:28:01 -0500

    Update DISK.h

--- DISK.h
+++ DISK.h
@@ -36,10 +36,42 @@ public:
         archivo.write("\0",1);
         archivo.close();
     }
-    void Escribir_sector() {
-
+    void Escribir_sector(long long superficie, long long pista, long long sector, const char* buffer) 
+    {
+        
+        long long offset = Desplazamiento(superficie, pista, sector);
+        fstream archivo("disco.dat", ios::in | ios::out | ios::binary);
+        if (!archivo) {
+            cout << "ERROR: No se pudo abrir el disco" << endl;
+            return;
+        }
+        archivo.seekp(offset);
+        archivo.write(buffer, capacidad_sector);
+        archivo.close();
+        cout << "Datos escritos en sector (" << superficie << "," << pista << "," << sector << ")" << endl;
     }
-    void Leer_Sector();
+    void Leer_Sector(long long superficie, long long pista, long long sector, char* buffer)
+    {
+        
+        long long offset = Desplazamiento(superficie, pista, sector);
+        fstream archivo("disco.dat", ios::in | ios::binary);
+        if (!archivo) {
+            cout << "ERROR: No se pudo abrir el disco" << endl;
+            return;
+        }
+        archivo.seekg(offset);
+        archivo.read(buffer, capacidad_sector);
+        archivo.close();   
+        
+    }
+    long long getCapacidadSector() 
+    {
+        return capacidad_sector;
+    }
+    long long getTotalSectores() 
+    {
+        return nro_superficies * nro_pistas * nro_sectores;
+    }
 
 private:
     long long nro_platos,
